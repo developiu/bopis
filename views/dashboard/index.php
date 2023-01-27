96a8dd89b2c833d1ec0eb2efbb1db7bedbc4156c
francesco <info@developiu.it>
2023-01-27 17:12:52 +0100

    inserita la guida utente

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,3 +1,116 @@
 <?php $this->layout('layouts/layout') ?>
 
-BREVE GUIDA UTENTE
+
+<h2>Overview</h2>
+<p>
+    Amazon ha recentemente lanciato un programma chiamato “BOPIS”, Buy online pick up in store, che consente ai clienti
+    di acquistare su Amazon e di recarsi presso il negozio/magazzino locale del venditore per ritirare la merce.
+</p>
+<p>
+    Questo articolo illustra il processo di configurazione di X-PORT POS per partecipare con successo al programma Amazon BOPIS.
+</p>
+
+
+<h2>Creazione di un Amazon store in X-PORT POS</h2>
+
+<b>*Inserire breve video*</b>
+
+<p>
+    Per creare un Amazon Store in X-POR POS, segui i passi indicati qui sotto.
+</p>
+<ul>
+    <li>1. Accedi alla sezione Store.</li>
+    <li>2. Compila debitamente tutte le sezioni. Le sezioni indicate con * sono obbligatorie per poter correttamente
+        partecipare al programma.</li>
+    <li>3. Clicca il pulsante Registra. Una volta cliccato sul pulsante visualizzerai un messaggio..</li>
+</ul>
+
+
+<h2>Gestione dei prodotti dedicati al programma Amazon BOPIS</h2>
+
+<b>*Inserire breve video*</b>
+
+<p>1. Per abilitare un prodotto al programma Amazon BOPIS:</p>
+
+<ul>
+    <li>A. Clicca sulla sezione Prodotti.</li>
+    <li>B. Clicca sul pulsante Carica e seleziona il file csv debitamente compilato. Puoi comodamente scaricare il
+        template del file csv da questo link.
+    </li>
+</ul>
+
+<p>2. Per aggiornare le quantita’ in X-PORT POS.</p>
+
+<ul>
+    <li>A. Scannerizzando ogni singolo ordine* evaso posizionandoti nella sezione Vendite, e premendo “Invio”, le
+        quantita’per articolo nella sezione Prodotti verranno automaticamente aggiornate.
+    </li>
+    <li>B. Nel caso in cui si intenda apportare variazione manuali, bastera’posizionarsi nel menu’principale Prodotti,
+        selezionare la casella Quantita’ di ogni singolo prodotto e digitare la quantita’disponibile. Per
+        creare/aggiornare campi aggiungitivi rispetto a quella relativa alle quantita’, sara’necessario cliccare
+        su’Aggiorna/Crea prodotto, scannerizzare il codice a barre del prodotto desiderato e compilare le informazioni
+        richieste nella mashera d’interfaccia.
+    </li>
+</ul>
+<p>Completate le operazioni di cui ai punti 2A e 2B le quantita’di magazzino in X-PORT POS risulteranno aggiorante.
+    Tuttavia, affinche’anche le quantita’in Amazon seller central risultino aggiornate, sara’necessario cliccare sul
+    pulsante Aggiorna quantita’ da X-PORT POS.</p>
+
+
+<p>N.B. E’molto importante, ai fini di una corretta tenuta delle quantita’di magazzino disponibili per il programma
+    Amazon BOPIS, che il seller effettui una decisione a riguardo delle quantita’da dedicare al progamma, in quanto in
+    base a questa scelta varieranno gli ordini da scannerizzare o meno. Le opzioni di configurazione suggerite sono
+    tre:</p>
+
+<ul>
+    <li><p>dedicare tutte le quantita’di prodotto disponibili in azienda anche al programma Amazon BOPIS. In questo
+            caso, il seller dovra’scannerizzare tutti gli ordini su’qualsiasi canale in quanto l’inventario totale
+            disponibile e’condiviso con il programma Amazon BOPIS.</p>
+        <p>Esempio. Se in totale un seller ha 10 prodotti disponibili in magazzino, e tali quantita’ sono in vendita in
+            egual misura su’tutti i canali, al ricevimento di un ordine su’canale fisico o piattaforma e-commerce non
+            Amazon, la quantita’disponibile sara’9. Tale variazione di quantita’deve essere comunicata ed aggiornata
+            su’X-PORT POS, e quindi su’Amazon seller central, scannerizzando tale prodotto venduto in X-PORT POS.</p>
+    </li>
+    <li><p>dedicare una parte delle quantita’ totali di prodotto disponibili in azienda ad Amazon, sia programma BOPIS
+            che programma consegna. In questo caso il seller dovra’scannerizzare i soli ordini Amazon, sia BOPIS che
+            consegna, ma non dovra’scannerizzare i prodotti derivanti da ordini su altri canali in quanto non inficiano
+            sulle quantita’disponibili per Amazon.</p>
+        <p>Esempio. Se in totale un seller ha 10 prodotti disponibili in magazzino, puo’decidere di dedicarne solo 4 al
+            canale Amazon (in egual misura per BOPIS e consegna) e 6 ad altri canali, creando cosi inventari separati.
+            Al
+            ricevimento di un ordine su’canale fisico o qualsiasi canale extra Amazon (con disponibilita’quindi di 6
+            articoli), il seller non dovra’effettuare alcuna scannerizzazione, in quanto le vendite sui canali extra
+            Amazon
+            non inficiano sulle quantita’disponibili (4) per il programma Amazon BOPIS.</p>
+    </li>
+    <li><p>dedicare una parte delle quantita’ totali di prodotto disponibili in azienda al solo programma Amazon BOPIS.
+            In questo caso il seller dovra’scannerizzare i soli ordini Amazon BOPIS ma non dovra’scannerizzare i
+            prodotti
+            derivanti da ordini da altri canali come Amazon consegna o store fisico, in quanto non inficiano sulle
+            quantita’disponibili per il programma Amazon BOPIS.</p>
+        <p>Esempio. Se in totale un seller ha 10 prodotti disponibili in magazzino, puo’decidere di dedicarne solo 4 al
+            canale Amazon BOPIS e 6 ad altri canali, tra i quali Amazon consegna, creando cosi inventari separati. Al
+            ricevimento di un ordine su’canale fisico o canale Amazon consegna per esempio (con disponibilita’quindi di
+            6
+            articoli), il seller non dovra’effettuare alcuna scannerizzazione, in quanto le vendite sui canali extra
+            Amazon
+            BOPIS non inficiano le quantita’disponibili (4) per il programma Amazon BOPIS.</p>
+    </li>
+</ul>
+
+<h2>Gestione degli ordini</h2>
+
+<p>Per visualizzare i nuovi ordini Amazon BOPIS, cliccare su’Ordini in entrata > Importa Ordini</p>
+
+<p>Selezionare quindi ogni singolo ordine nel riquadro Seleziona e dal menu’a Tendina selezionare lo stato dell’ordine
+    tra le varie possibili opzioni: 1) Accettato; 2) Cancellato</p>
+
+<p>Selezionato lo stato desiderato dalle opzioni di cui sopra, sara’necessario notificare Amazon cliccando sul pulsante
+    “Notifica Amazon”</p>
+
+<p>Gli ordini contrassegnati con la status “Confermato”, saranno elencati nel menu’Ordini confermati. Per ogni ordine
+    sara’quindi necessario aggiornare lo stato selezionando tra: 1) Pronto per la consegna; 2) Consegnato; 3) Rifiuto di
+    ritiro.</p>
+
+<p>Selezionato lo stato desiderato dalle opzioni di cui sopra, sara’necessario notificare Amazon cliccando sul pulsante
+    “Notifica Amazon”</p>
